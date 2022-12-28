8e8e935d4211a51531256e70937a184437d09356
unknown <raihanarif778@gmail.com>
2022-12-29 00:43:23 +0600

    add task api added

--- index.jsx
+++ index.jsx
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 require("dotenv").config();
 
 //middleware
@@ -12,28 +14,54 @@ app.use(express.json());
 //dbName: tmSystem
 //dbPasseord: evxGZ5W1mvyK5piZ
 
-const { MongoClient } = require("mongodb");
-
 // Replace the uri string with your connection string.
-const uri =
-  "mongodb+srv://tmSystem:evxGZ5W1mvyK5piZ@<cluster-url>?retryWrites=true&w=majority";
 
-const client = new MongoClient(uri);
+const uri = `mongodb+srv://tmSystem:DHlmLecUOKvoakpS@cluster0.jz1qjld.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
     const usersCollection = client.db("tmSystem").collection("tmUsers");
+    const tasksCollection = client.db("tmSystem").collection("allTasks");
 
-    // Query for a movie that has the title 'Back to the Future'
-    app.get("/users", async (req, res) => {});
-    const query = { title: "Back to the Future" };
-    const movie = await movies.findOne(query);
+    //post method for save users data.
+    app.post("/users", async (req, res) => {
+      const query = req.body;
+      const result = await usersCollection.insertOne(query);
+      res.send(result);
+    });
 
-    console.log(movie);
+    //post method for add tasks in database.
+    app.post("/all-task", async (req, res) => {
+      const query = req.body;
+      const result = await tasksCollection.insertOne(query);
+      console.log("add task", result);
+      res.send(result);
+    });
+
+    //delete method for delete tasks in database.
+    app.delete("/all-task/:id", async (req, res) => {
+      const query = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      result = await tasksCollection.deleteOne(filter);
+      res.send(result);
+    });
+    // get method for task finding
+
+    app.get("/all-task", async (req, res) => {
+      const query = {};
+      const result = await tasksCollection.find(query).toArray();
+      res.send(result);
+    });
   } finally {
   }
 }
-run();
+run().catch((err) => console.log(err));
 
 //mondodb crud end
 
